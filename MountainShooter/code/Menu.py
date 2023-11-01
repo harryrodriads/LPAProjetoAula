1e36d6ef69ff227e7d8915ed82d3fe5a9ecd3aae
Harry Rodrigues <harryrodriads@gmail.com>
2023-11-01 14:42:13 -0300

    Aula sobre criação e movimento de entidades

--- MountainShooter/code/Menu.py
+++ MountainShooter/code/Menu.py
@@ -24,6 +24,7 @@ class Menu:
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", (255, 128, 0), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", (255, 128, 0), ((WIN_WIDTH / 2), 70))
             pygame.display.flip()
 
 
