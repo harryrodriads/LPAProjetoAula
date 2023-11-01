00f4cb06cb92f2385c4fa10489ccfe14b1e7294b
Harry Rodrigues <harryrodriads@gmail.com>
2023-11-01 16:22:02 -0300

    Aula sobre criação e movimento de entidades

--- MountainShooter/code/Menu.py
+++ MountainShooter/code/Menu.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
 import pygame
 from pygame import Surface, Rect
 from pygame.ftfont import Font
-from Const import WIN_WIDTH
+from Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -21,10 +23,35 @@ class Menu:
     def run(self):
         pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
+        menu_option = 0
         while True:
+            # Desenhar na Tela
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", (255, 128, 0), ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", (255, 128, 0), ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            # Verificar Eventos
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, ((WIN_WIDTH / 2), 200 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 30 * i))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:  # Testar se alguma tecla foi pressionada
+                    if event.key == pygame.K_DOWN:  # Tecla Seta pra Baixo Pressionada
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:  # Tecla Seta pra Cima Pressionada
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:  # Tecla ENTER Pressionada
+                        return MENU_OPTION[menu_option]
             pygame.display.flip()
-
-
